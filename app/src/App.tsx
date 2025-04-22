f72388bb1e9270f974a73542862d6b80113dd278
Sabarish V S <Sabarish_7@protonmail.com>
2025-04-22 23:16:23 +0530

    added vercel analytics

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,12 @@
 import HomePage from "./pages/HomePage";
 import ScrollProgress from "./pages/scrollBar";
-
+import { Analytics } from "@vercel/analytics/next";
 
 function App() {
   return (
     <div className="h-min ">
       <HomePage />
+      <Analytics />
       <ScrollProgress />
     </div>
   );
